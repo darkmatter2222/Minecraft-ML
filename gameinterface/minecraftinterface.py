e06a29dc9b50e275404caafdfa8d4299bc0a287e
ryan susman <ryansusman@gmail.com>
2021-01-31 13:17:01 -0600

    interface building

--- gameinterface/minecraftinterface.py
+++ gameinterface/minecraftinterface.py
@@ -1,4 +1,14 @@
 import win32gui
+import keyboard
+import time
+import pyautogui
+from PIL import Image
+
+
+target_size = (640, 480)
+target_location = (0, 0)
+screen_grab_location_offset = (8, 0)
+screen_grab_size_offset = (-16, -8)
 
 
 def window_enumeration_handler(hwnd, top_windows):
@@ -28,7 +38,47 @@ class Win10MinecraftApp:
                             "Make sure Windows 10 Minecraft is running and "
                             "your world is loaded")
         else:
-            win32gui.MoveWindow(self.minecraft['app'][0], 600, 480, 0, 0, True)
+            win32gui.MoveWindow(self.minecraft['app'][0], target_location[0], target_location[1],
+                                target_size[0], target_size[1], True)
+            x0, y0, x1, y1 = win32gui.GetWindowRect(self.minecraft['app'][0])
+            w = x1 - x0  # width
+            h = y1 - y0  # height
+            self.minecraft['location'] = (x0, y0)
+            self.minecraft['size'] = (w, h)
+            win32gui.SetForegroundWindow(self.minecraft['app'][0])
+
+        print(f"Minecraft Windows 10 Found, Loc:{self.minecraft['size']}")
+
+    def send_keystroke(self, key_instructions):
+        if not isinstance(key_instructions, list):
+            raise Exception(f"Expected list of keystroke actions, got {type(key_instructions)}")
+
+        for key_instruction in key_instructions:
+            if key_instruction['action'] == 'press':
+                keyboard.press(key_instruction['key'])
+            elif key_instruction['action'] == 'press_and_release':
+                keyboard.press(key_instruction['key'])
+                time.sleep(0.1)
+                keyboard.release(key_instruction['key'])
+            elif key_instruction['action'] == 'release':
+                keyboard.release(key_instruction['key'])
+            else:
+                raise Exception("Invalid Instruction to act upon")
+            
+    def is_pressed(self, key):
+        return keyboard.is_pressed(key)
+
+    def get_screen(self):
+        screen_shot = pyautogui.screenshot(region=(
+        self.minecraft['location'][0] + screen_grab_location_offset[0],
+        self.minecraft['location'][1] + screen_grab_location_offset[1],
+        self.minecraft['size'][0] + screen_grab_size_offset[0],
+        self.minecraft['size'][1] + screen_grab_size_offset[1])).convert('L').convert('RGB')
+
+            
+        
+
+
 
 
 
