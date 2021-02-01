a7e7ee46b2bab9f1c18af14490009c898744364e
ryan susman <ryansusman@gmail.com>
2021-01-31 20:10:50 -0600

    optimize

--- bot/trainer.py
+++ bot/trainer.py
@@ -47,19 +47,19 @@ model = tf.keras.Sequential([
     tf.keras.layers.Conv2D(64, 3, activation='relu'),
     tf.keras.layers.MaxPooling2D(2),
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dropout(0.1),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(4, activation=tf.nn.sigmoid)
 ])
 
 model.compile(loss='categorical_crossentropy',
-              optimizer=RMSprop(lr=0.001),
+              optimizer='adamgit add',
               metrics=['accuracy'])
 
 history = model.fit(
       train_generator,
       steps_per_epoch=train_generator.samples/train_generator.batch_size,
-      epochs=2,
+      epochs=10,
       validation_data=validation_generator,
       validation_steps=validation_generator.samples/validation_generator.batch_size,
       verbose=1)
