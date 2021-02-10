5941672ef1ac2f14b569250dc41937286ce92d38
ryan susman <ryansusman@gmail.com>
2021-02-09 21:13:24 -0600

    cleanup

--- helpers/generategif.py
+++ helpers/generategif.py
@@ -3,8 +3,8 @@ from PIL import Image
 from random import sample
 
 # filepaths
-fp_in = "N:\\minecraft-ml\\selfjumping\\training\\space\\*.png"
-fp_out = "N:\\minecraft-ml\\selfjumping\\training\\image.gif"
+fp_in = "N:\\minecraft-ml\\selfjumping\\training\\none\\*.png"
+fp_out = "N:\\minecraft-ml\\selfjumping\\training\\imagenone.gif"
 
 # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
 img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
